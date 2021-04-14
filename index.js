aebbe1af5d9c17538e7042194c18caacf657ea97
Evan Sellers <sellersew@gmail.com>
2021-04-14 18:08:59 -0400

    home page

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express = require( "express" );
 const path    = require( "path" );
 const app     = express();
 const GAMES   = require( "./decks/decks.json" );
+const HOME    = require( "./decks/home.json" )
 const shuffle = require( "shuffle-array" );
 
 app.set( "views", path.join( __dirname, "views" ) );
 app.set( "view engine", "ejs" );
 app.use( "/public", express.static( "public" ) );
+
+// Process Home Page
+HOME.forEach( section => {
+	section[ "decks" ] = [];
+	section.decksID.forEach( deck => {
+		GAMES.forEach( game => {
+			if ( game.id == deck ) section[ "decks" ].push( game );
+		});
+	});
+});
 
 
 app.get( "/deck/:id", ( req, res ) => {
@@ -38,6 +49,10 @@ app.get( "/deck/:id", ( req, res ) => {
 
 app.get( "/search", ( req, res ) => {
 	res.render( "search", { games: GAMES } );
+});
+
+app.get( "/", ( req, res ) => {
+	res.render( "index", { sections: HOME } );
 });
 
 app.listen( 3000 );
