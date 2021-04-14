68882c354b2de82dafd07cb05cb906480ff5406a
Evan Sellers <sellersew@gmail.com>
2021-04-14 14:20:06 -0400

    added peak

--- public/framework/index.js
+++ public/framework/index.js
@@ -1,30 +1,16 @@
 window.addEventListener("scroll", (event) => {
+    console.log("hi")
     let height = "innerHeight" in window ? window.innerHeight : document.body.offsetHeight;
     let scoll = window.scrollY;
-    let ribbon = document.querySelector(".navigation");
+    let ribbon = document.querySelectorAll(".navigation");
 
     if (scoll > height * 0.05) {
-        ribbon.classList.add("isScrolled");
+        ribbon.forEach( elm => {
+            elm.classList.add("isScrolled");
+        });
     } else {
-        if (ribbon.classList.contains("isScrolled")) {
-            ribbon.classList.remove("isScrolled");
-        }
+        ribbon.forEach( elm => {
+            elm.classList.remove("isScrolled");
+        });
     }
-});
-
-
-window.addEventListener( "load", () => {
-    document.querySelector( ".peak.container" ).addEventListener( "scroll", (event) => {
-        let height = "innerHeight" in window ? window.innerHeight : document.body.offsetHeight;
-        let scoll = window.scrollY;
-        let ribbon = document.querySelector( ".peak.container.navigation" );
-    
-        if (scoll > height * 0.05) {
-            ribbon.classList.add("isScrolled");
-        } else {
-            if (ribbon.classList.contains("isScrolled")) {
-                ribbon.classList.remove("isScrolled");
-            }
-        }
-    });
 });
