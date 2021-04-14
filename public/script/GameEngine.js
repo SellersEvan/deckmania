2eb95fd69cdc77ef8edb60bc19d722bbbdb2d334
Evan Sellers <sellersew@gmail.com>
2021-04-14 12:15:08 -0400

    added flipping animation

--- public/script/GameEngine.js
+++ public/script/GameEngine.js
@@ -3,37 +3,49 @@
 
 
 function nextCard() {
-    let active = document.querySelector( ".question.question-active" );
-    if ( active == null ) {
-        document.querySelector( ".question" ).classList.add( "question-active" );
-    } else {
-        active.classList.remove( "question-active" );
-        let number   = Number( active.dataset[ "card" ] ) + 1;
-        let property = `.question[data-card="${ number }"]`;
-        let element  = document.querySelector( property );
+    let card   = document.querySelector( ".question-container" );
+    card.classList.add( "flip" );
+    setTimeout( () => {
+        card.classList.remove( "flip" );
 
-        if ( element == null ) {
+        let active = document.querySelector( ".question.question-active" );
+        if ( active == null ) {
             document.querySelector( ".question" ).classList.add( "question-active" );
         } else {
-            element.classList.add( "question-active" );
+            active.classList.remove( "question-active" );
+            let number   = Number( active.dataset[ "card" ] ) + 1;
+            let property = `.question[data-card="${ number }"]`;
+            let element  = document.querySelector( property );
+    
+            if ( element == null ) {
+                document.querySelector( ".question" ).classList.add( "question-active" );
+            } else {
+                element.classList.add( "question-active" );
+            }
         }
-    }
+    }, 300 );
 }
 
 function prevCard() {
-    let active = document.querySelector( ".question.question-active" );
-    if ( active == null ) {
-        document.querySelector( ".question:last-child" ).classList.add( "question-active" );
-    } else {
-        active.classList.remove( "question-active" );
-        let number   = Number( active.dataset[ "card" ] ) - 1;
-        let property = `.question[data-card="${ number }"]`;
-        let element  = document.querySelector( property );
+    let card   = document.querySelector( ".question-container" );
+    card.classList.add( "flip" );
+    setTimeout( () => {
+        card.classList.remove( "flip" );
 
-        if ( element == null ) {
+        let active = document.querySelector( ".question.question-active" );
+        if ( active == null ) {
             document.querySelector( ".question:last-child" ).classList.add( "question-active" );
         } else {
-            element.classList.add( "question-active" );
+            active.classList.remove( "question-active" );
+            let number   = Number( active.dataset[ "card" ] ) - 1;
+            let property = `.question[data-card="${ number }"]`;
+            let element  = document.querySelector( property );
+    
+            if ( element == null ) {
+                document.querySelector( ".question:last-child" ).classList.add( "question-active" );
+            } else {
+                element.classList.add( "question-active" );
+            }
         }
-    }
+    }, 300 );
 }
